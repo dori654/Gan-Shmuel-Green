ded1de35434ea93a83709efec8093e191aed24dd
Yairdamri <162902326+Yairdamri@users.noreply.github.com>
2025-07-26 20:28:16 +0300

    Update main.py

--- Weight/app/main.py
+++ Weight/app/main.py
@@ -12,24 +12,6 @@ def health():
     except:
         return "Failure", 500
 
-# GET /weight?from=t1&to=t2&filter=f
-@app.route("/weight", methods=["GET"], strict_slashes=False)
-def get_weight():
-    start_time = request.args.get('from')  # yyyymmddhhmmss Default is today at 00:00:00.
-    end_time = request.args.get('to')      # yyyymmddhhmmss Default is now.
-    directions= request.args.get('filter') # default is "in,out,none"
-
-# recive data from DB, calculate the following,
-# returns an array of json objects (data), one per weighing (batch NOT included):
-#   data=[{
-#   "id": <id>,
-#   "direction": in/out/none,
-#   "bruto": <int>, //in kg
-#   "neto": <int> or "na" // na if some of containers have unknown tara
-#   "produce": <str>,
-#   "containers": [ id1, id2, ...]  }]
-    return jsonify(data), 200
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
