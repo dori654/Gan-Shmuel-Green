0e3a3ef04067a3789b47d6111d4569b5410a87db
kobi-nesher <kobitech.n@gmail.com>
2025-07-27 14:48:07 +0300

    remove entry comments

--- Devops/entry.sh
+++ Devops/entry.sh
@@ -4,14 +4,3 @@ docker rm -f $(docker ps -aq) 2>/dev/null || true
 docker build -t ci .
 docker run -d -it --name ci_container -p 8080:8080 ci
 
-# curl -X POST http://172.17.0.2:8080/trigger   -H "Content-Type: application/json"   -H "X-G
-# itHub-Event: push"   -d '{
-#     "ref": "refs/heads/main",
-#     "repository": {
-#       "name": "example-repo"
-#     }
-#   }'
-
-    # print(f"Received event: {event_type}")
-    # return jsonify({'status': 'Ignored'}), 200  # Default response
-    #
