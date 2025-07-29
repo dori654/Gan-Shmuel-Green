61f9418c445952ce5356731d7ed2ac1acf323e34
kobi-nesher <kobitech.n@gmail.com>
2025-07-29 10:28:35 +0300

    test change

--- Devops/app/ci_manager.py
+++ Devops/app/ci_manager.py
@@ -121,4 +121,4 @@ def run_ci_pipeline(payload):
         notify_slack(f"🔥 CI failed for `{branch}`: {str(e)}")
         return "CI failed"
 
-#mini change to test the CI pipeline
+#mini change to test the CI
