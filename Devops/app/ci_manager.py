eff6af1c5c7c3da8660aa95cfc622aee18cc1933
Dori Fourer <dori.fourer654@gmail.com>
2025-07-29 16:45:45 +0300

    fix routes

--- Devops/app/ci_manager.py
+++ Devops/app/ci_manager.py
@@ -94,23 +94,23 @@ def run_ci_pipeline(payload):
 
 
             #build weight image
-            build_weight_result = run_cmd("Build weight image", "docker compose -f ../Weight/docker-compose.yml up -d --build")
+            build_weight_result = run_cmd("Build weight image", "docker compose -f /builder/Weight/docker-compose.yml up -d --build")
             if build_weight_result.returncode != 0:
                 raise Exception("Failed to build weight image")
             
 
             #test weight image
-            test_weight_result = run_cmd("Run Pytest on Weight", "docker compose -f ../Weight/docker-compose.test.yml exec -T app pytest")
+            test_weight_result = run_cmd("Run Pytest on Weight", "docker compose -f /builder/Weight/docker-compose.yml exec -T weight pytest")
             if test_weight_result.returncode != 0:
                 raise Exception("Pytest failed for Weight service")
 
             #build billing image
-            build_billing_result = run_cmd("Build billing image", "docker compose -f ../Billing/docker-compose.yml up -d --build")
+            build_billing_result = run_cmd("Build billing image", "docker compose -f /builder/Billing/docker-compose.yml up -d --build")
             if build_billing_result.returncode != 0:
                 raise Exception("Failed to build billing image")
 
             #test billing image
-            test_billing_result = run_cmd("Run Pytest on Billing", "docker compose -f ../Billing/docker-compose.test.yml exec -T app pytest")
+            test_billing_result = run_cmd("Run Pytest on Billing", "docker compose -f /builder/Billing/docker-compose.yml exec -T billing pytest")
             if test_billing_result.returncode != 0:
                 raise Exception("Pytest failed for Billing service")
 
